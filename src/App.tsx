75a0996f67c34c86be461a9d28a991837a88dbc7
eatspaint <cef2010@gmail.com>
2024-04-30 15:18:05 -0700

    update links

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { DevTo, GitHub, IG, LinkedIn, Twitter } from "./icons";
+import { GitHub, IG, LinkedIn } from "./icons";
 
 const SOURCE = [
   { href: "https://eatspaint.com", title: "Resume & Tech stack" },
@@ -24,45 +24,33 @@ const SOURCE = [
     title: "Code Art (p5.js)",
   },
   {
-    href: "https://soundcloud.com/eatspaint/sets/2020-2021-collected",
+    href: "https://eatspaint.bandcamp.com/album/2020-2021-collected",
     title: "Experimental electronic music",
   },
 ];
 
 function App() {
   return (
-    <div className="App">
-      <div className="content">
-        <img
-          className="pfp"
-          src="/pfp.png"
-          alt="mirror selfie in a cathedral"
-        />
-        <h1>eatspaint online</h1>
-        <div className="links">
-          {SOURCE.map(({ href, title }) => (
-            <a href={href} key={href}>
-              {title}
-            </a>
-          ))}
-        </div>
-        <div className="icons">
-          <a href="https://twitter.com/eatspaint">
-            <Twitter />
+    <div className="content">
+      <img className="pfp" src="/pfp.png" alt="mirror selfie" />
+      <h1>eatspaint online</h1>
+      <div className="links">
+        {SOURCE.map(({ href, title }) => (
+          <a href={href} key={href}>
+            {title}
           </a>
-          <a href="https://github.com/eatspaint">
-            <GitHub />
-          </a>
-          <a href="https://dev.to/eatspaint">
-            <DevTo />
-          </a>
-          <a href="https://instagram.com/cef2010">
-            <IG />
-          </a>
-          <a href="https://linkedin.com/in/christianedwardfuller/">
-            <LinkedIn />
-          </a>
-        </div>
+        ))}
+      </div>
+      <div className="icons">
+        <a href="https://github.com/eatspaint">
+          <GitHub />
+        </a>
+        <a href="https://instagram.com/_eatspaint">
+          <IG />
+        </a>
+        <a href="https://linkedin.com/in/christianedwardfuller/">
+          <LinkedIn />
+        </a>
       </div>
     </div>
   );
